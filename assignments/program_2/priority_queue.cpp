3ba476aea700054598c72e014450783372a724e4
Colton Boyd <35539405+coltonboyd87@users.noreply.github.com>
2018-05-04 13:40:52 -0500

    Update priority_queue.cpp

--- assignments/program_2/priority_queue.cpp
+++ assignments/program_2/priority_queue.cpp
@@ -10,7 +10,7 @@
 //the priority. The program will print out all popped off animals off of     //
 //queue. And then after that will print out all the names remaining in the   //
 //queue in alphabetical order.                                               //
-//               							     //														 //
+//               							     //								 //														 //
 //Files: input_data.txt, priority_queue.txt				     //
 //Email: coltonboyd14@yahoo.com 					     //
 //Semester: Spring 2018 						     //						 
@@ -30,12 +30,12 @@
 using namespace std;
 
 //Struct that contains our Node elements for our 
-//linked list. 
+//linked list.
 struct Node
 {
 	string val;
-	Node *next;
-	Node()
+	Node* next;
+	Node() // initializes node to empty and next pointer to null
 	{
 		val = " ";
 		next = NULL;
@@ -46,6 +46,8 @@ struct Node
 		next = NULL;
 	}
 };
+
+
 
 /**
 * class Queue:
@@ -62,19 +64,16 @@ struct Node
 *  - bool empty() : will return whether or not the current list is empty
 * or non empty.
 */
+
 class Queue
 {
 private:
-	Node *Front;
-	Node *Rear;
-	string word_1;
-	string word_2;
-
+	Node* Front;
+	Node* Rear;
 public:
 	/**
 	* Queue
 	* constructor
-	*
 	* params:
 	*     - none
 	*/
@@ -83,65 +82,19 @@ public:
 		Front = Rear = NULL;
 	}
 
-	/** Method name: void lengthBasedPQueue
-	*
-	* Description: Will sort the words in the queue based the
-	* length of the word.
-	*
-	* Params: None
-	*
-	* Returns: void
-	*
-	*
-	*/
-
-	void lengthBasedPQueue()
-	{
-		Node *temp = Front;
-		if (empty())
-		{             // handles the empty case, doesnt have to return a value 
-			return;     // because it is a void type. 
-		}
-
-		while (temp->next != NULL)
-		{	                                // This puts words in an order to be swapped. 
-			string word_1 = temp->val;
-			string word_2 = temp->next->val;
-		}
-
-		if (word_1.length() > word_2.length())
-		{	                                // Case in which word 1 is longer than
-			temp->val = word_2;             // word 2. 
-			temp->next->val = word_1;
-		}
-
-		else if (word_1.length() == word_2.length())
-		{
-			if (word_1 > word_2)
-			{
-				temp->val = word_2;        // Case in which word 1 is in equal length 
-				temp->next->val = word_1;  // of word 2. 
-			}
-		}
-		temp = temp->next;
-	}
-
-
-	/** Method name: string pop
-	*
-	* Description: will return name of animal at the front of list. Will display error
-	* message if queue is somehow empty.
-	*
-	* Params: None
-	*
-	* Returns: string
-	*
+	/**
+	* Function pop:
+	*      Pops an item off the queue.
+	* Params:
+	*      none
+	* Returns
+	*      string
 	*/
 	string pop()
 	{
 		if (!empty())
 		{
-			Node *temp = Front;   // temp pointer so we can delete node
+			Node* temp = Front;   // temp pointer so we can delete node
 			string v = temp->val; // grab value out of node
 			Front = Front->next;  // move pointer to next node
 			delete temp;          // give mem back to system
@@ -151,66 +104,24 @@ public:
 		{
 			cout << "error: cannot pop off empty queue." << endl;
 			return " ";
-		}               // Error message indicating an empty queue. 
-	}
-
-	/** Method name: void push
-	*
-	* Description: pushes a new animals name onto non-empty queue.
-	*
-	* Params: string
-	*
-	* Returns: None because it is a void type.
-	*
-	*/
-	void push(string v)
-	{
-		Node *n = new Node(v);
-
-		// handle the empty case
-		if (empty())
-		{
-			Front = n;
-			Rear = n;
-		}
-		else
-		{
-			// not empty add to rear
-			Rear->next = n;
-			Rear = n;
 		}
 	}
 
-	/** Method name: bool empty
-	*
-	* Description: will return whether or not the current list is empty
-	* or non empty.
-	*
-	* Params: None
-	*
-	* Returns: boolean, T or F.
-	*
-	*/
-	bool empty()
-	{
-		return (Front == NULL && Rear == NULL);
-	}
 
 
-	/** Method name: bool pop
-	*
-	* Description: will pop name of animal off of queue.
-	*
-	* Params: string
-	*
-	* Returns: boolean, T or F.
-	*
+	/**
+	* Function pop:
+	*      Popss an item off the queue.
+	* Params:
+	*      [string] v - value of item being pushed
+	* Returns
+	*      void
 	*/
 	bool pop(string &v)
 	{
 		if (!empty())
 		{
-			Node *temp = Front;   // temp pointer so we can delete node
+			Node* temp = Front;   // temp pointer so we can delete node
 			string t = temp->val; // grab value out of node
 			Front = Front->next;  // move pointer to next node
 			delete temp;          // give mem back to system
@@ -223,128 +134,205 @@ public:
 			return false;
 		}
 	}
-	////////////////////////////////////////////
-	//                                        //
-	//Functions to be used in main function:  //
-	//                                        //
-	//string word                             //
-	//oFile                                   //
-	//                                        //
-	//                                        //
-	////////////////////////////////////////////
 
-	/** Fuction name: string word
-	*
-	* Description: Will return the first item in the queue
-	*
-	* Params: None
-	*
-	* Returns: string
-	*
+	/**
+	* Function push:
+	*      Pushes an item onto the queue.
+	* Params:
+	*      [string] val - value of item being pushed
+	* Returns
+	*      void
+	*/
+	void push(string v)
+	{
+		Node* n = new Node(v);
+		// handle the empty case
+		if (empty())
+		{
+			Front = n;
+			Rear = n;
+		}
+		else
+		{	// not empty add to rear
+			Rear->next = n;
+			Rear = n;
+		}
+	}
+
+	/**
+	* Function empty:
+	*      Checks if the list is empty.
+	* Params:
+	*      none
+	* Returns
+	*      bool
+	*/
+	bool empty()
+	{
+		return (Front == NULL);
+	}
+
+	/**
+	* Function print:
+	*      Prints every item still on the queue.
+	* Params:
+	*      none
+	* Returns
+	*      void
+	*/
+	void print()
+	{
+		Node* temp = Front;
+		while (temp)
+		{
+			cout << temp->val << "->";
+			temp = temp->next;
+		}
+		cout << '\n';
+	}
+
+	/**
+	* Function lengthBasedPQueue:
+	*      Sorts the items in the queue based on length.
+	* Params:
+	*      none
+	* Returns
+	*      void
+	*/
+	void lengthBasedPQueue()
+	{
+		Node *temp = Front;
+
+		if (empty())
+		{
+			return;
+		}
+
+		while (temp->next != NULL)
+		{
+			string word1 = temp->val;
+			string word2 = temp->next->val;
+
+			if (word1.length() > word2.length())
+			{	//swaps the nodes' values 
+				temp->val = word2;
+				temp->next->val = word1;
+			}
+
+			// if TIE - handles words of same length by placing them
+			// alphabetically
+			else if (word1.length() == word2.length())
+			{
+				if (word1 > word2)
+				{
+					temp->val = word2;
+					temp->next->val = word1;
+				}
+			}
+			temp = temp->next;
+		}
+	}
+
+	/**
+	* Function word:
+	*      Returns the front value of the list for easy access before popping item off list.
+	* Params:
+	*      none
+	* Returns
+	*      string
 	*/
 	string word()
 	{
-		while (!empty())  //This is a check for the first item in queue 
-		{                 // and will ultimately return for its use. 
+		while (!empty())
+		{
 			return Front->val;
 		}
 		return "";
 	}
 
-	/** Fuction name: oFile
-	*
-	* Description: Will print remaining to an outfile, based on the word
-	* length priority within the queue.
-	*
-	* Params: oFile ofstream
-	*
-	* Returns: void
-	*
+	/**
+	* Function outfileList:
+	*      Prints list node value and number on list to output file.
+	* Params:
+	*      outfile
+	* Returns
+	*      void
 	*/
-
-	void oFile(ofstream &ofile)
+	void outfileList(ofstream &outfile)
 	{
-		int num = 1;
+		int itemsoutfiled = 1;
 		Node *temp = Front;
 		while (temp)
 		{
-			ofile << num++ << " " << temp->val << '\n';
+			outfile << itemsoutfiled++ << " " << temp->val << '\n';
 			temp = temp->next;
 		}
 	}
+//Main Function 
 };
-
-
-//////////////////////
-//                  //
-//Main Function     //
-//                  //
-//////////////////////
 
 int main()
 {
-	//Variable intilizing
+	int number = 1;// for counting the number of items
+						// popped off the list.
 	Queue Q;
-	string animal, command;
-	int num_popped = 1;
-	int node_Count = 0;
+	string animal, command; // command is either push or pop
+	int nodecount = 0;      // keeps track of the number of nodes
+							// in the list
 
-	//Data Type to create an instance of a text file to be able to access
-	//strings in the input_data.txt file. 
-	//Also creates an output file called priority_out.txt to output 
-	//animals names. 
-	ifstream ifile;
-	ifile.open("input_data.txt");
+	fstream infile;
+	infile.open("input_data.txt");
+	ofstream outfile;
+	outfile.open("priority_out.txt");// items still on Queue when
+									 // program ends
+	outfile << "Animals Popped off the Queue:" << '\n' << '\n';
 
-
-
-	ofstream ofile;
-	ofile.open("priority_out.txt"); //defining our ofile 
-	ofile << "Animals Popped off the Queue:" << '\n' << '\n';
-
-	//Loop to go through and process input_data.txt file. 
-	while (ifile >> command)
+	while (infile >> command)
 	{
-		//If statement to handle the case of the push function being commanded in 
-		//input text file. This will run the push method defined earlier in our 
-		//Class Q. 
 		if (command == "push")
 		{
-			ifile >> animal;
-			Q.push(animal); //Push animal onto queue
-			node_Count++;
-		}
+			infile >> animal;
+			Q.push(animal);
+			nodecount++;
+		} // end of if
 
-		//Else if statement to handle the case of the pop function being commanded 
-		//in input text file. This will run the pop method defined earlier in our 
-		//class Q. 
 		else if (command == "pop")
 		{
-			if (Q.word() != "")
+			if (Q.word() != "") // only prints and
+										  // increments number
+										  // it the list is not empty
 			{
-				ofile << num_popped++ << " " << Q.word() << '\n';
+				outfile << number++ << " " << Q.word() << '\n';
 			}
 			Q.pop(animal);
-			node_Count--;
-			if (node_Count < 0)
+			nodecount--;
+			if (nodecount < 0)  // avoids making nodecount negative
 			{
-				node_Count = 0;
-			}
-		}
+				nodecount = 0;   // in for loop
+			} // end of if
+		} // end of else if
 
-		if (node_Count > 1)
-		{
-			for (int i = 0; i < node_Count - i; i++)
+		if (nodecount > 1)    // only sorts list when there is more
+		{                     // than one item
+			for (int i = 0; i < nodecount - i; i++) // loops the sort
+													// function
+													// enough times
+													// for the
+													// shortest items
+													// in list to
+													// move to the
+													// front
 			{
-				Q.lengthBasedPQueue();  //This is where we implement our method to 
-			}                         //sort our animal names based on priority. 
-		}
-	}
-	//ofile 
-	ofile << '\n' << "Animals Remaining on the Queue (In Order of priority):" << '\n' << '\n';
+				Q.lengthBasedPQueue();
+			} // end for loop
+		} // end of if
+	} // end of while
 
-	Q.oFile(ofile);
-	ifile.close(); //commands to close ifile and ofile files. 
-	ofile.close();
+	outfile << '\n' << "Animals Remaining on the Queue (in order of priority):" << '\n' << '\n';
+
+	Q.outfileList(outfile);
+
+	infile.close();
+	outfile.close();
 	return 0;
 }
